080b3807a1a58052543c1181c79042e477e8037e
R K <mowers-01jostle@icloud.com>
2023-12-17 15:47:58 +0200

    Updated Point.java file

--- Lesson8/src/Point.java
+++ Lesson8/src/Point.java
@@ -2,43 +2,43 @@ public class Point {
     private int _x;
     private int _y;
 
+    // Constructors
     public Point(int x, int y)
     {
         _x = x;
         _y = y;
     }
-
     public Point(Point other)
     {
         _x = other._x;
         _y = other._y;
     }
-
     public Point(int x)
     {
         _x = x;
     }
 
+    // Getters
     public int get_x()
     {
         return _x;
     }
-
     public int get_y()
     {
         return _y;
     }
 
+    // Setters
     public void set_x(int x)
     {
         _x = x;
     }
-
     public void set_y(int y)
     {
         _y = y;
     }
 
+    // Methods
     public boolean equals(Point other)
     {
         if ((_x == other._x) && (_y == other._y))
@@ -49,5 +49,11 @@ public class Point {
         {
             return false;
         }
+    }
+
+    public void move (int deltaX, int deltaY)
+    {
+        _x += deltaX;
+        _y += deltaY;
     }
 }
